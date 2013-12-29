f69dca09c430aaaca4d1ea72ed10ac1f3491a284
Zoi <Zoi@Zoi-PC>
2013-12-29 17:42:09 +0300

    temp commit 1 cread class StereoOffCommand

---
+++ src/com/patterns/command/command/StereoOffCommand.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.patterns.command.command;
+
+/**
+ *
+ * @author Zoi
+ */
+public class StereoOffCommand implements Command {
+    Stereo stereo;
+    public StereoOffCommand( Stereo stereo){
+        this.stereo = stereo;
+    }
+    public void execute(){
+        stereo.off();
+    }
+//    public void undo(){
+//        stereo.on();
+//    }
+}
