8130272c4073aa1b8ba82408895ce4a6ed0f78e3
Zoi <Zoi@Zoi-PC>
2013-12-29 17:55:10 +0300

    temp commit 1 cread class TVOnCommand

--- src/com/patterns/command/command/Stereo.java
+++ src/com/patterns/command/command/Stereo.java
@@ -11,23 +11,27 @@ package com.patterns.command.command;
  * @author Zoi
  */
 public class Stereo {
+    String name;
+    public Stereo(String name){
+        this.name = name;
+    }
     void on(){
-        System.out.println( " stereo is On");
+        System.out.println(name + " stereo is On");
     }
     void off(){
-        System.out.println( " stereo is Off");
+        System.out.println(name + " stereo is Off");
     }
     void setCD(){
-        System.out.println( " stereo is set for CD input");
+        System.out.println(name + " stereo is set for CD input");
     }
     void setDVD(){
-        System.out.println( " stereo is set for DVD input");
+        System.out.println(name + " stereo is set for DVD input");
     }
     void setRadio(){
-        System.out.println( " stereo is set for Radio input");
+        System.out.println(name + " stereo is set for Radio input");
     }
     void setVolume( int i){
-        System.out.println( " stereo volume set to " + i);
+        System.out.println(name + " stereo volume set to " + i);
     }
     
 }
