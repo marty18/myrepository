e1048b33ba9f7513bd758d19137e367b4375f027
Zoi <Zoi@Zoi-PC>
2013-12-29 17:49:16 +0300

    temp commit 1 cread class MacroCommand

---
+++ src/com/patterns/command/command/MacroCommand.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.patterns.command.command;
+
+/**
+ *
+ * @author Zoi
+ */
+public class MacroCommand implements Command {
+    Command[] commnads;
+    
+    public MacroCommand( Command[] commands){
+        this.commnads = commands;
+    }
+    
+    public void execute(){
+        for( int i = 0; i < commnads.length; i++){
+            commnads[i].execute();
+        }
+    }
+}
