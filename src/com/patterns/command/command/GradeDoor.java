b9fe31824f0cc6d17ef7502bea1d3f14b531f162
Zoi <Zoi@Zoi-PC>
2013-12-29 17:21:06 +0300

    temp commit 1

---
+++ src/com/patterns/command/command/GradeDoor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.patterns.command.command;
+
+/**
+ *
+ * @author Zoi
+ */
+public class GradeDoor {
+    
+    void up() {System.out.println("Grade Door is Open");}
+    
+}
