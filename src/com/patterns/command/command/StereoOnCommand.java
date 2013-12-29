1896ac7a1e87bed305c0e4ff7f0447787dc3b08c
Zoi <Zoi@Zoi-PC>
2013-12-29 17:36:59 +0300

    temp commit 1 cread class StereoOnCommand

---
+++ src/com/patterns/command/command/StereoOnCommand.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.patterns.command.command;
+
+/**
+ *
+ * @author Zoi
+ */
+public class StereoOnCommand implements Command {
+    Stereo stereo;
+    public StereoOnCommand(Stereo stereo){
+        this.stereo = stereo;
+    }
+    
+    public void execute(){
+        stereo.on();
+        stereo.setCD();
+        stereo.setVolume(11);
+    }
+    
+}
