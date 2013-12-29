3ef63860e69f0658e499685ac267f065f750d80f
Zoi <Zoi@Zoi-PC>
2013-12-29 16:47:33 +0300

    temp commit 1

---
+++ src/com/patterns/command/command/LightOnCommand.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.patterns.command.command;
+
+/**
+ *
+ * @author Zoi
+ */
+public class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+    this.light = light;
+    }
+
+    public void execute() {
+       light.on();
+    }
+}
