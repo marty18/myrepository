29ad77430be2c2b5b2871f6036088ec0674b0e2c
Zoi <Zoi@Zoi-PC>
2013-12-29 17:21:59 +0300

    temp commit 1

---
+++ src/com/patterns/command/command/NoCommand.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.patterns.command.command;
+
+/**
+ *
+ * @author Zoi
+ */
+public class NoCommand implements Command {
+    public void execute(){};
+    
+}
